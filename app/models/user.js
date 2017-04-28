687a9f28ae3b35e06f5f8ef3573e4475ba458c2b
khadijatariq <khadijatariqzaman@gmail.com>
2017-04-28 15:40:39 +0500

    Schemas for instructor and student info

--- app/models/user.js
+++ app/models/user.js
@@ -3,17 +3,39 @@ var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  email: { type: String, required: true, lowercase: true, unique: true },
-  password: { type: String, required: true },
-  state: { type: String, required: true, lowercase: true }
+	email: { type: String, required: true, lowercase: true, unique: true },
+	password: { type: String, required: true },
+	state: { type: String, required: true, lowercase: true }
+});
+
+var StudentSchema = new Schema({
+  	email: { type: String, required: true, lowercase: true, unique: true },
+  	fname: {type: String, required: true},
+  	lname: {type: String, required: true},
+  	gender: {type: String, required: true, lowercase: true},
+  	field: {type: String, required: true, lowercase: true},
+  	gpa: {type: Number, reuqired: true},
+  	cgpa: {type: Number, reuqired: true}
+});
+
+var InstructorSchema = new Schema({
+  	email: { type: String, required: true, lowercase: true, unique: true },
+  	fname: {type: String, required: true},
+  	lname: {type: String, required: true},
+  	gender: {type: String, required: true, lowercase: true},
+  	field: {type: String, required: true, lowercase: true},
+  	room: {type: String, reuqired: true},
+  	ext: {type: String, reuqired: true}
 });
 
 UserSchema.pre('save', function(next) {
-  var user = this;
-  bcrypt.hash(user.password, null, null, function(err, hash) {
-    user.password = hash;
-    next();
-  });
+	var user = this;
+	bcrypt.hash(user.password, null, null, function(err, hash) {
+		user.password = hash;
+	    next();
+	});
 });
 
-module.exports = mongoose.model('Users', UserSchema);
+module.exports.User = mongoose.model('Users', UserSchema);
+module.exports.Student = mongoose.model('Students', StudentSchema);
+module.exports.Instructor = mongoose.model('Instructors', InstructorSchema);
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,8 +1,8 @@
-var User = require('../models/user');
+var schemas = require('../models/user');
 
 module.exports = function(router) {
 	router.post('/register', function(req, res){
-		var user = new User();
+		var user = new schemas.User();
 		user.email = req.body.email;
 		user.password = req.body.password;
 		user.state = req.body.state;
@@ -11,6 +11,27 @@ module.exports = function(router) {
 				res.json({success: false, message: 'Account with this email already exists!'});
 			} else {
 				res.json({success: true, message: 'Data received.'});
+				if (req.body.state == 'student'){
+					var student = new schemas.Student();
+					student.email = req.body.email;
+					student.fname = req.body.fname;
+					student.lname = req.body.lname;
+					student.gender = req.body.gender;
+					student.field = req.body.field;
+					student.gpa = 3.854;
+					student.cgpa = 3.909;
+					student.save();
+				} else if (req.body.state == 'instructor'){
+					var instructor = new schemas.Instructor();
+					instructor.email = req.body.email;
+					instructor.fname = req.body.fname;
+					instructor.lname = req.body.lname;
+					instructor.gender = req.body.gender;
+					instructor.field = req.body.field;
+					instructor.room = req.body.room;
+					instructor.ext = req.body.extension;
+					instructor.save();
+				}
 			}
 		});
 	});
